8c7c73c7234c4bd7f1341cd6b623e21852ee6ca7
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-17 20:47:42 +0200

    Fixed some field name typos : )

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,15 +1,15 @@
 class SqlQueries:
     songplay_table_insert = ("""
         SELECT
-                md5(events.sessionId || events.start_time) songplay_id,
+                md5(events.sessionid || events.start_time) songplay_id,
                 events.start_time, 
-                events.userId, 
+                events.userid, 
                 events.level, 
                 songs.song_id, 
                 songs.artist_id, 
-                events.sessionId, 
+                events.sessionid, 
                 events.location, 
-                events.userAgent
+                events.useragent
                 FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
             FROM staging_events
             WHERE page='NextSong') events
