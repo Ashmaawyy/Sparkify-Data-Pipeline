357f12ade22c05bf7fdfd2a4d71c587bb2b975d6
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-17 10:34:46 +0200

    Better view for imports : )

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -2,9 +2,12 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
+from airflow.operators import \
+    StageToRedshiftOperator, \
+    LoadFactOperator, \
+    LoadDimensionOperator, \
+    DataQualityOperator
+from plugins.helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
