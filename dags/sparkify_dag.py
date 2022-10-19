52775cdfa56c51d1595a4f1264bbdcfb48abc54a
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-19 14:41:12 +0200

    Better drawing : )

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -40,17 +40,17 @@ load_songplays_fact_table = LoadFactOperator(
     dag=dag
 )
 
-load_user_dimension_table = LoadDimensionOperator(
+load_users_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
     dag=dag
 )
 
-load_song_dimension_table = LoadDimensionOperator(
+load_songs_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
     dag=dag
 )
 
-load_artist_dimension_table = LoadDimensionOperator(
+load_artists_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
     dag=dag
 )
@@ -68,13 +68,13 @@ run_data_quality_checks = DataQualityOperator(
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
 #                                                                                            
-#                                                                                              ======> load_songs_dimention_table =========== \\
-#                      ======> satge_events_to_redshift                                     || ======> load_users_dimention_table ============
-#                   ||                                 \\                                  ||                                                   \\
-#   start_operator                                        =====> load_songplays_fact_table                                                         ========> run_data_quality_checks ===========> end_operator
-#                   \\                                 ||                                  \\                                                   ||
-#                      ======> stage_songs_to_redshift                                      \\ =======> load_artists_dimention_table =========
-#                                                                                              =======> load_time_dimention_table =========== ||
+#                                                                                              ======> load_songs_dimention_table ===========> 
+#                      ======> satge_events_to_redshift                                        ======> load_users_dimention_table ============>
+#                   ||                                 \\                                  ||                                                     \\
+#   start_operator                                        =====> load_songplays_fact_table                                                           ========> run_data_quality_checks ===========> end_operator
+#                   \\                                 ||                                  \\                                                     ||
+#                      ======> stage_songs_to_redshift                                         =======> load_artists_dimention_table =========>
+#                                                                                              =======> load_time_dimention_table ===========>
 #
 
 # First stage in DAG
@@ -86,15 +86,15 @@ stage_events_to_redshift >> load_songplays_fact_table
 stage_songs_to_redshift >> load_songplays_fact_table
 
 # Third stage in DAG
-load_songplays_fact_table >> load_song_dimension_table
-load_songplays_fact_table >> load_user_dimension_table
-load_songplays_fact_table >> load_artist_dimension_table
+load_songplays_fact_table >> load_songs_dimension_table
+load_songplays_fact_table >> load_users_dimension_table
+load_songplays_fact_table >> load_artists_dimension_table
 load_songplays_fact_table >> load_time_dimension_table
 
 # Fourth stage in DAG
-load_song_dimension_table >> run_data_quality_checks
-load_user_dimension_table >> run_data_quality_checks
-load_artist_dimension_table >> run_data_quality_checks
+load_songs_dimension_table >> run_data_quality_checks
+load_users_dimension_table >> run_data_quality_checks
+load_artists_dimension_table >> run_data_quality_checks
 load_time_dimension_table >> run_data_quality_checks
 
 # Fifth (and final) stage in DAG
