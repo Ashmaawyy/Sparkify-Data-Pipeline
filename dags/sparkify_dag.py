48a65cf2482221d5237b40a57e04c2137e5cf8ab
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-19 13:18:31 +0200

    Drew the DAG : )

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -35,7 +35,7 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     dag=dag
 )
 
-load_songplays_table = LoadFactOperator(
+load_songplays_fact_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag
 )
@@ -60,9 +60,37 @@ load_time_dimension_table = LoadDimensionOperator(
     dag=dag
 )
 
-run_quality_checks = DataQualityOperator(
+run_data_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
     dag=dag
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+#                                                                                            
+#                                                                                          ------> load_songs_dimention_table ------------
+#                    ------> satge_events_to_redshift                                      ------> load_users_dimention_table               \
+#                   |                                 \                                  |                                          
+#   start_operator                                     -----> load_songplays_fact_table                                                       ----------> run_data_quality_checks ---------> end_operator
+#                   \                                 |                                  \                                         
+#                    ------> stage_songs_to_redshift                                      -------> load_artists_dimention_table            |
+#                                                                                         -------> load_time_dimention_table -------------
+#
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_fact_table
+stage_songs_to_redshift >> load_songplays_fact_table
+
+load_songplays_fact_table >> load_song_dimension_table
+load_songplays_fact_table >> load_user_dimension_table
+load_songplays_fact_table >> load_artist_dimension_table
+load_songplays_fact_table >> load_time_dimension_table
+
+load_song_dimension_table >> run_data_quality_checks
+load_user_dimension_table >> run_data_quality_checks
+load_artist_dimension_table >> run_data_quality_checks
+load_time_dimension_table >> run_data_quality_checks
+
+run_data_quality_checks >> end_operator
