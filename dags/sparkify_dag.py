796afa168b24a83fc7abd5a2e5e7f090b853ddd6
Ashmaawyy <al_ashmawy@outlook.com>
2022-10-24 00:01:03 +0200

    Added StageToRedshiftOperator() parameters : )

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -27,7 +27,12 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='stage_events',
-    dag=dag
+    dag=dag,
+    redshift_conn_id = 'redshift',
+    aws_credentials_id = 'aws_credentials',
+    table = 'events',
+    s3_bucket = 'udacity-dend',
+    s3_key = 'log_data'
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
